1636b07ca5707817163dd77cb5d0a5c900cbc690
matt-dray <mwdray@gmail.com>
2019-01-10 09:00:38 +0000

    Add additional inputs and test filtering

--- app.R
+++ app.R
@@ -1,34 +1,100 @@
-library(shiny)
-library(shinydashboard)
-library(leaflet)
-library(DT)
-library(dplyr)
+# Deer-vehicle collisions in Scotland, 2000 to 2017: a Shiny app
+# UI and Server
+# Matt Dray
+# January 2019
 
+
+# Global ------------------------------------------------------------------
+
+# Load packages
+library(shiny)  # interactive app framework
+library(shinydashboard)  # layout
+library(leaflet)  # interactive map
+library(DT)  # interactive table
+library(dplyr)  # data manipulation
+
+# Read pre-prepared data
 dvc <- readRDS("data/dvc_sample.RDS")
 
+
+# UI ----------------------------------------------------------------------
+
+
 ui <- dashboardPage(
-    dashboardHeader(title = "Deer-vehicle collisions in Scotland, 2000 to 2017"),
+    
+    dashboardHeader(
+        title = "Deer-vehicle collisions in Scotland, 2000 to 2017"
+    ),  # end dashboardHeader
+    
     dashboardSidebar(
         selectInput(
+            inputId = "input_year", 
+            label = "Year",
+            choices = unique(dvc$year),
+            multiple = TRUE
+        ),
+        selectInput(
+            inputId = "input_month", 
+            label = "Month",
+            choices = unique(dvc$inc_month),
+            multiple = TRUE
+        ),
+        dateRangeInput(
+            inputId = "input_daterange",
+            label = "Date range",
+            start = min(dvc$inc_date, na.rm = TRUE),
+            end = max(dvc$inc_date, na.rm = TRUE),
+            min = min(dvc$inc_date, na.rm = TRUE),
+            max = max(dvc$inc_date, na.rm = TRUE),
+            startview = "decade",
+            #weekstart = 1,
+            separator = " to "
+        ),
+        selectInput(
             inputId = "input_la", 
-            label = "Choose a local authority",
+            label = "Local authority",
             choices = unique(dvc$localautho),
             multiple = TRUE
+        ),
+        selectInput(
+            inputId = "input_road", 
+            label = "Road",
+            choices = unique(dvc$road_no),
+            multiple = TRUE
+        ),
+        selectInput(
+            inputId = "input_species", 
+            label = "Deer species",
+            choices = unique(dvc$deer_speci),
+            multiple = TRUE
         )
-    ),
+    ),  # end dashboardSidebar
+    
     dashboardBody(
         fluidRow(
-            box(leafletOutput("output_map"), width = 12),
+            box(leafletOutput("output_map"), width = 12),  # full width
             box(dataTableOutput("output_table"), width = 12)
         )
     )  # end dashboardBody
-)  # end dashboardPage
+    
+)  # end of ui dashboardPage
+
+
+# Server ------------------------------------------------------------------
+
 
 server <- function(input, output) {
     
+    # Interactive map with Leaflet
     output$output_map <- renderLeaflet({
         dvc %>%
-            filter(localautho %in% input$input_la) %>% 
+            filter(
+                year %in% input$input_year
+                # inc_month %in% input$input_month,
+                # localautho %in% input$input_la,
+                # road_no %in% input$input_road,
+                # deer_speci %in% input$input$species
+            ) %>% 
             leaflet() %>% 
             addProviderTiles(providers$OpenStreetMap) %>% 
             addAwesomeMarkers(
@@ -38,18 +104,21 @@ server <- function(input, output) {
                 )
             )
     })  # end of renderLeaflet
-
+    
+    # Interactive map with Leaflet
     output$output_table <- renderDataTable({
-        
-        dvc %>%
-            filter(localautho %in% input$input_la) %>% 
+        dvc %>% 
+            filter(
+                year %in% input$input_year
+            ) %>% 
             select(inc_date, year, inc_month, localautho, road_no, deer_speci) %>% 
             datatable()
-        
     })  # end of renderDataTable
     
-}  # end of server
-    
+}  # end of server function
+
+
+# Run ---------------------------------------------------------------------
 
 
 shinyApp(ui, server)
--- global.R
+++ global.R
@@ -1,49 +1,87 @@
-# Deer-vehicle collisions in Scotland, 2000 to 2017
+# Deer-vehicle collisions in Scotland, 2000 to 2017: a Shiny app
 # Data preparation
-# Dec 2018
+# Jan 2018
 # Matt Dray
 
-# load packages
-library(flexdashboard)
-library(dplyr)
-library(stringr)
-library(janitor)
-library(sf)
-library(lubridate)
-library(crosstalk)
-library(leaflet)
-library(DT)
-library(shiny)
-library(shinythemes)
+# Load packages -----------------------------------------------------------
 
-# read and prep data
-dvc_read <- st_read(
-  "data/DVC_SCOTLAND_ESRI/DVC_SCOTLAND.shp",
-  stringsAsFactors = FALSE  # read factor columns as character
+
+# Data manipulation and cleaning
+library(dplyr)  # tidy data manipulation
+library(stringr)  # string manipulation
+library(janitor)  # misc tidy data manipulation
+library(lubridate)  # dealing with dates and times
+library(sf)  # geography
+
+# tool structure
+library(flexdashboard)  # layout of the tool (pages, frames, etc)
+
+# htmlwidgets
+library(crosstalk)  # for allowing htmlwidgets to interact with shared data
+library(leaflet)  # interactive maps
+library(DT)  # interactive tables
+
+
+# Read and wrangle data ---------------------------------------------------
+
+
+dvc_read <- st_read(  # read geographic data
+  "data/DVC_SCOTLAND_ESRI/DVC_SCOTLAND.shp",  # shapefile stored in repo
+  stringsAsFactors = FALSE  # read factor columns as character columns
 ) %>% 
   st_transform(crs = 4326) %>%  # transform coords to latlong
-  clean_names() %>% mutate_if(is.character, tolower) %>% 
+  clean_names() %>% mutate_if(is.character, tolower) %>%  # simplify strings
   mutate(
     inc_date = ymd(inc_date),  # convert to datetime
-    # clean up names
-    deer_speci = if_else(deer_speci == "desppnk", "unknown", deer_speci),
+    inc_month = case_when(
+      inc_month == 1 ~ "JAN",
+      inc_month == 2 ~ "FEB",
+      inc_month == 3 ~ "MAR",
+      inc_month == 4 ~ "APR",
+      inc_month == 5 ~ "MAY",
+      inc_month == 6 ~ "JUN",
+      inc_month == 7 ~ "JUL",
+      inc_month == 8 ~ "AUG",
+      inc_month == 9 ~ "SEP",
+      inc_month == 10 ~ "OCT",
+      inc_month == 11 ~ "NOV",
+      inc_month == 12 ~ "DEC",
+      TRUE ~ "NA"
+    ),
+    # clean up names (incomplete)
+    deer_speci = if_else(deer_speci %in% c("desppnk", "uncl.", "nk"), "unknown", deer_speci),
     localautho = if_else(localautho == "perth & kinross", "perth_and_kinross", localautho),
     road_no = if_else(road_no %in% c("unalloc", "notallocated", "notalloc", "u"), "unknown", road_no),
     road_no = if_else(str_detect(road_no, "x") == TRUE, "unknown", road_no)
-  )
+  ) %>% 
+  # final name clean
+  mutate(localautho = str_replace_all(localautho, "_", " ")) %>%  # insert spaces
+  mutate_at(vars(deer_speci, localautho, road_no), tools::toTitleCase)  # To Title Case
 
 # extract latlong cols from sf geometry and bind back to df
 dvc_xy <- as.data.frame(st_coordinates(dvc_read))
+dvc <- bind_cols(dvc_read, dvc_xy) %>% rename(latitude = X, longitude = Y)
 
-dvc <- bind_cols(
-  dvc_read,
-  dvc_xy
-) %>%
-  rename(latitude = X, longitude = Y)
 
-# create sample for testing
-dvc_sample <- sample_n(dvc, 200)
+# Sample ------------------------------------------------------------------
 
-# Save the objects
-# saveRDS(dvc, "data/dvc.RDS")
-# saveRDS(dvc_sample, "data/dvc_sample.RDS")
+
+# create sample for testing the app (50 per year)
+dvc_sample <- dvc %>% 
+  group_by(year) %>%  # within each year
+  sample_n(50)  # random selection
+
+
+# Shared data -------------------------------------------------------------
+
+
+# create shared data object for crosstalk
+# dvc_sd <- SharedData$new(dvc)
+dvc_sd <- SharedData$new(dvc_sample)
+
+
+# Save objects ------------------------------------------------------------
+
+
+saveRDS(dvc, "data/dvc.RDS")
+saveRDS(dvc_sample, "data/dvc_sample.RDS")
